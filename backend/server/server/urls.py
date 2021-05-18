8d8dfa2100b65ab5402f203779c2853f57fc99fd
Sahithya Cherukuri <46041721+SahithyaCherukuri@users.noreply.github.com>
2021-05-19 00:37:43 +0530

    Jupyter Notebook

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -16,6 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+
+from apps.endpoints.views import EndpointViewSet
+from apps.endpoints.views import MLAlgorithmViewSet
+from apps.endpoints.views import MLAlgorithmStatusViewSet
+from apps.endpoints.views import MLRequestViewSet
+from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r"^api/v1/", include(router.urls)),
 ]
+urlpatterns += endpoints_urlpatterns
